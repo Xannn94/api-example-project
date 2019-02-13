5c2df404d00e8c3f3e633001f8ed48607c346471
Alex Khan <axiles94@gmail.com>
2019-02-13 22:39:50 +0600

    add product, order and order items migrations

---
+++ database/migrations/2019_02_13_155600_create_order_item_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('order_id');
+            $table->unsignedInteger('product_id');
+            $table->unsignedInteger('quantity');
+            //product_price and product_title need if user delete product which use in exist orders
+            $table->unsignedInteger('product_price');
+            $table->string('product_title');
+            $table->timestamps();
+
+            $table->foreign('order_id')
+                ->references('id')
+                ->on('orders')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_item');
+    }
+}
